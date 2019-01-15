6e71a06480d9c07cac78195030fb1eb9eb64f2ce
Puoliväli Tuomas B <puolival@dx7-h916-01.biocenter.helsinki.fi>
2019-01-15 16:04:10 +0200

    Test program ready. The function definitions in fidelity need to be changed next to match interface.

--- test.py
+++ test.py
@@ -1,13 +1,27 @@
+from fidelity import compute_weighted_operator
+
 import mne
-from mne import read_forward_solution
+from mne import read_forward_solution, read_labels_from_annot
 from mne.datasets import sample
 from mne.minimum_norm import read_inverse_operator
 
 """Read forward and inverse operators from disk."""
-fpath = sample.data_path() + '/MEG/sample'
+fpath = sample.data_path()
+fpath_meg = fpath + '/MEG/sample'
 
-fname_forward = fpath + '/sample_audvis-meg-oct-6-fwd.fif'
-fname_inverse = fpath + '/sample_audvis-meg-oct-6-meg-inv.fif'
+fname_forward = fpath_meg + '/sample_audvis-meg-oct-6-fwd.fif'
+fname_inverse = fpath_meg + '/sample_audvis-meg-oct-6-meg-inv.fif'
 
 fwd = read_forward_solution(fname_forward)
 inv = read_inverse_operator(fname_inverse)
+
+"""Read labels from FreeSurfer annotation files."""
+subject = 'sample'
+subjects_dir = fpath + '/subjects'
+parcellation = 'aparc.a2009s'
+
+labels = read_labels_from_annot(subject, subjects_dir=subjects_dir,
+                                parc=parcellation)
+
+"""Compute the fidelity-weighted inverse operator."""
+fid_inv = compute_weighted_operator(fwd, inv, labels)
