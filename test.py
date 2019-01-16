cfa91e5082623a86d8ad66fb10d075b27bf9ba4a
Puoliväli Tuomas B <puolival@dx7-h916-01.biocenter.helsinki.fi>
2019-01-16 11:22:21 +0200

    Force fixed orientation for now.

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from fidelity import compute_weighted_operator
 
 import mne
-from mne import read_forward_solution, read_labels_from_annot
+from mne import (convert_forward_solution, read_forward_solution,
+                 read_labels_from_annot)
 from mne.datasets import sample
 from mne.minimum_norm import read_inverse_operator
 
@@ -15,6 +16,9 @@ fname_inverse = fpath_meg + '/sample_audvis-meg-oct-6-meg-inv.fif'
 fwd = read_forward_solution(fname_forward)
 inv = read_inverse_operator(fname_inverse)
 
+"""Force fixed source orientation mode."""
+fwd_fixed = convert_forward_solution(fwd, force_fixed=True)
+
 """Read labels from FreeSurfer annotation files."""
 subject = 'sample'
 subjects_dir = fpath + '/subjects'
@@ -24,4 +28,4 @@ labels = read_labels_from_annot(subject, subjects_dir=subjects_dir,
                                 parc=parcellation)
 
 """Compute the fidelity-weighted inverse operator."""
-fid_inv = compute_weighted_operator(fwd, inv, labels)
+fid_inv = compute_weighted_operator(fwd_fixed, inv, labels)
