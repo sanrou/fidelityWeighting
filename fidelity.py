Fixed off-by-one error. Also adding n_labels // 2 after the loop was making -1 values 199, which would then need to be subsequently corrected. It is therefore easier to just add the n_labels // 2 within the loop, since the structure is initialized with -1's.

fidelity.py
@@ -190,10 +190,8 @@ def _extract_operator_data(fwd, inv, labels_parc, method):
     # find sources that belong to the right HS labels
     for l, label in enumerate(labels_parc[n_labels//2:n_labels]):
         for v in label.vertices:
-            src_ident_rh[np.where(vert_rh == v)] = l
+            src_ident_rh[np.where(vert_rh == v)] = l + (n_labels // 2)
 
-    # Fix numbers, so that lh and rh are not identical.
-    src_ident_rh = src_ident_rh + (n_labels // 2) - 1
     src_identities = np.concatenate((src_ident_lh, src_ident_rh))
 
     # Extract forward matrix.